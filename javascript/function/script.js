231709217624485f222a74d3947b7252d2123658
nndz865 <watonyk122@gmail.com>
2025-10-09 07:09:11 +0700

    ada error

---
+++ javascript/function/script.js
@@ -0,0 +1,13 @@
+nama = "nanda";
+kelas = "XI RPL";
+absen = "07";
+nomor = "2347239578";
+
+document.getElementById("nama").innerHTML = nama;
+document.getElementById("kelas").innerHTML = kelas;
+document.getElementById("absen").innerHTML = absen;
+document.getElementById("nomor").innerHTML = nomor;
+
+function tekan() {
+namaSaya.innerHTML = "nama";
+}
