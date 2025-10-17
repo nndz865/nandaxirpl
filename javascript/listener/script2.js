85b55f1797f37035e2214366cf96ebac804371c3
nndz865 <watonyk122@gmail.com>
2025-10-17 09:16:39 +0700

    ada error

---
+++ javascript/listener/script2.js
@@ -0,0 +1,11 @@
+const display = document.getElementById('display'); 
+let expr = '';
+document.body.addEventListener('click',  function (e)  { 
+    if(e.target.tagName !== 'BUTTON') 
+    return; 
+    const val = e.target.textContent;
+     if(val === '=') { 
+        try { expr = eval(expr); } 
+     catch { expr = 'Error'; } 
+    } else if(val === 'C') { expr = ''; }
+     else { expr += val; } display.value = expr; });
