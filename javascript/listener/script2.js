4136f47af9c6f13da029445515ce6cccdce15e78
nndz865 <watonyk122@gmail.com>
2025-10-23 07:11:37 +0700

    ada error

--- javascript/listener/script2.js
+++ javascript/listener/script2.js
@@ -1,11 +1,32 @@
-const display = document.getElementById('display'); 
-let expr = '';
-document.body.addEventListener('click',  function (e)  { 
-    if(e.target.tagName !== 'BUTTON') 
-    return; 
-    const val = e.target.textContent;
-     if(val === '=') { 
-        try { expr = eval(expr); }
-     catch { expr = 'error'; }
-    } else if(val === 'C') { expr = ''; }
-     else { expr += val; } display.value = expr; });
+const angka1 = document.getElementById('angka1');
+const angka2 = document.getElementById('angka2');
+const hasil = document.getElementById('hasil');
+
+const tambah = document.getElementById('tambah');
+const kurang = document.getElementById('kurang');
+const kali = document.getElementById('kali');
+const bagi = document.getElementById('bagi');
+
+tambah.addEventListener('click', function() {
+   const a = parseFloat(angka1.value) || 0;
+   const b = parseFloat(angka2.value) || 0;
+   hasil.innerHTML = a + b;
+});
+
+kurang.addEventListener('click', function() {
+   const a = parseFloat(angka1.value) || 0;
+   const b = parseFloat(angka2.value) || 0;
+   hasil.innerHTML = a - b;
+});
+
+kali.addEventListener('click', function() {
+   const a = parseFloat(angka1.value) || 0;
+   const b = parseFloat(angka2.value) || 0;
+   hasil.innerHTML = a * b; 
+});
+
+bagi.addEventListener('click', function() {
+   const a = parseFloat(angka1.value) || 0;
+   const b = parseFloat(angka2.value) || 0;
+   hasil.innerHTML = a / b;
+});
